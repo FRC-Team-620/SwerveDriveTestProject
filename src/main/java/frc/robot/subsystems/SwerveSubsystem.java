f701a0532ef45c778f566232e9c442b98fa18680
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2022-08-16 18:10:39 -0400

    First commit

---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveSubsystem extends SubsystemBase {
+    /** Creates a new ExampleSubsystem. */
+  public SwerveSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
