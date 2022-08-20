1b40a9739188839f45ef61af32765e58a03828be
m10653 <mcombsb1@gmail.com>
2022-08-20 18:10:00 -0400

    Code Clean up. Added some comments for next meeting.

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -15,29 +15,34 @@ public class SwerveSubsystem extends SubsystemBase {
 
   SwerveDriveKinematics swerveDriveKinematics;
 
-  /** Creates a new ExampleSubsystem. */
   Translation2d frontLeftLocation = new Translation2d(0.381, 0.381);
   Translation2d frontRightLocation = new Translation2d(0.381, -0.381);
   Translation2d backLeftLocation = new Translation2d(-0.381, 0.381);
   Translation2d backRightLocation = new Translation2d(-0.381, -0.381);
 
-  //ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
-  
-
   SwerveModuleState frontLeft = new SwerveModuleState();
   SwerveModuleState frontRight = new SwerveModuleState();
   SwerveModuleState backLeft = new SwerveModuleState();
   SwerveModuleState backRight = new SwerveModuleState();
-  
-  //var frontLeftOptimized = SwerveModuleState.optimize(frontLeft, new Rotation2d(turningEncoder.getDistance()));
+
+  // var frontLeftOptimized = SwerveModuleState.optimize(frontLeft, new
+  // Rotation2d(turningEncoder.getDistance()));
 
   public SwerveSubsystem() {
-    swerveDriveKinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+    swerveDriveKinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, backLeftLocation,
+        backRightLocation);
+    // TODO: Calculate Correct robot size based off of wheel locations. May want to
+    // add a constructor to the Swerve Vis that accepts a SwerveDriveKinematics
+    // object.
+
     swerveVisualizer = new SwerveVisualizer(2, 2);
   }
 
-  public void swerveDrive(double x, double y, double theta) {
-    ChassisSpeeds desiredSpeeds = new ChassisSpeeds(x, y, theta);
+  public void swerveDrive(double xMetersPerSec, double yMetersPerSec, double rotationRadPerSec) {
+
+    // Calculate what angle the wheels need to be and their speed based off our
+    // desired robot velocities.
+    ChassisSpeeds desiredSpeeds = new ChassisSpeeds(xMetersPerSec, yMetersPerSec, rotationRadPerSec);
     SwerveModuleState[] moduleStates = swerveDriveKinematics.toSwerveModuleStates(desiredSpeeds);
     frontLeft = moduleStates[0];
     frontRight = moduleStates[1];
@@ -45,15 +50,14 @@ public class SwerveSubsystem extends SubsystemBase {
     backRight = moduleStates[3];
   }
 
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    System.out.println(frontLeft.angle);
-    System.out.println(frontRight.angle);
-    System.out.println(backLeft.angle);
-    System.out.println(backRight.angle);
-    swerveVisualizer.update(frontLeft.angle, frontRight.angle, backLeft.angle, backRight.angle, new Pose2d(5, 5, new Rotation2d()));
+    // TODO: Replace Hardcoded position of robot with data from odometry.
+
+    // Display the current desired location of all swerve modules.
+    swerveVisualizer.update(frontLeft.angle, frontRight.angle, backLeft.angle, backRight.angle,
+        new Pose2d(5, 5, new Rotation2d()));
   }
 
   @Override
