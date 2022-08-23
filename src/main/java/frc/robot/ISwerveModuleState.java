4d46fa8a76523bc265575cce1b5dae29061643fc
1552937 <51300628+1552937@users.noreply.github.com>
2022-08-23 18:38:39 -0400

    Added Interface for SwerveModuleState

---
+++ src/main/java/frc/robot/ISwerveModuleState.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public interface ISwerveModuleState {
+
+    public void setDesiredState(SwerveModuleState state);
+    public SwerveModuleState getModuleState();
+    public SwerveModuleState getDesiredState();
+    public void resetEncoders();
+}
