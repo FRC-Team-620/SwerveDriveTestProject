2af3b48de5b720a522ba552b123e8bad6160d6c5
1552937 <51300628+1552937@users.noreply.github.com>
2022-08-25 19:58:43 -0400

    SNAPPPYYYYYY

--- src/main/java/frc/robot/swerve/SimSwerveModule.java
+++ src/main/java/frc/robot/swerve/SimSwerveModule.java
@@ -24,19 +24,21 @@ public class SimSwerveModule implements ISwerveModuleState {
         id = ID_TRACKER++;
         nTablesName = "SwerveModule" + id + "/";
         drivePID = new PIDController(Constants.driveKp, Constants.driveKi, Constants.driveKd);
-        anglePID = new ProfiledPIDController(Constants.angleKp, Constants.angleKi, Constants.angleKd, new TrapezoidProfile.Constraints(12.0*Math.PI, 12.0*Math.PI));
+        anglePID = new ProfiledPIDController(Constants.angleKp, Constants.angleKi, Constants.angleKd, new TrapezoidProfile.Constraints(100, 100));
         anglePID.enableContinuousInput(-Math.PI, Math.PI);
         drivePID.setIntegratorRange(-12, 12);
         anglePID.setIntegratorRange(-12, 12);
         sim = new SwerveModuleHardwareSim();
         SmartDashboard.putData(nTablesName + "Drive PID", drivePID);
         SmartDashboard.putData(nTablesName + "Caster PID", anglePID);
+        // SmartDashboard.putNumber("Turny Velocity", 12);
+        // SmartDashboard.putNumber("Turny Acceleration", 12);
     }
 
     @Override
     public void setDesiredState(SwerveModuleState state) {
-        // desiredState = SwerveModuleState.optimize(state, new Rotation2d(sim.getCasterAngleRad()));
-        desiredState = state;
+        desiredState = SwerveModuleState.optimize(state, new Rotation2d(sim.getCasterAngleRad()));
+        // desiredState = state;
         TrapezoidProfile.State goal = new TrapezoidProfile.State(MathUtil.angleModulus(desiredState.angle.getRadians()), 0);
         
         // Drive PID units are radians per second. 
@@ -67,11 +69,13 @@ public class SimSwerveModule implements ISwerveModuleState {
         
         sim.setWheelMotorVolts(driveVolts);
         sim.setCasterMotorVolts(angleVolts);
+        // anglePID.setConstraints(new TrapezoidProfile.Constraints(SmartDashboard.getNumber("Turny Velocity", 12), SmartDashboard.getNumber("Turny Acceleration", 12)));
         SmartDashboard.putNumber(nTablesName + "Drive Volts", driveVolts);
         SmartDashboard.putNumber(nTablesName + "Angle Volts", angleVolts);
         SmartDashboard.putNumber(nTablesName + "Desired Speed", desiredState.speedMetersPerSecond/Constants.wheelRadius);
         SmartDashboard.putNumber(nTablesName + "Desired Angle", MathUtil.angleModulus(desiredState.angle.getRadians()));
         SmartDashboard.putNumber(nTablesName + "Angle", MathUtil.angleModulus(sim.getCasterAngleRad()));
         SmartDashboard.putNumber(nTablesName + "Speed", sim.getWheelRadPerSec());
+        SmartDashboard.putNumber(nTablesName + "Angle Setpoint", anglePID.getSetpoint().position);
     }
 }
