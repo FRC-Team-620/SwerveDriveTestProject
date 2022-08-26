688569d793284f3440fdd2d1c996f05d793ebecd
Ava <bencurby80@gmail.com>
2022-08-26 14:07:31 -0400

    Working toggleable field oriented swerve.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.SwerveCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,7 +24,7 @@ public class RobotContainer {
   public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem(true);
 
   private final SwerveCommand autoCommand = new SwerveCommand(swerveSubsystem);
-
+  private final Joystick driverJoystick = new Joystick(0);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -34,7 +37,12 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(driverJoystick, 1).whenPressed(
+      new InstantCommand(() -> this.swerveSubsystem.setFieldOriented(
+        !this.swerveSubsystem.getFieldOriented())
+      ));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
